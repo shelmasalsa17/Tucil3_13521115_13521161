dbd95ebfe48769a6a62c394d56349914204530d0
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2023-04-07 23:57:28 +0700

    add:exception

--- src/helper.py
+++ src/helper.py
@@ -6,36 +6,42 @@ def eucleudianDistance(nodeAwal, nodeAkhir, coordinateDictionary):
     + (coordinateDictionary[nodeAwal]["long"] - coordinateDictionary[nodeAkhir]["long"]) ** 2) ** (0.5), 3)
     return distance
 def readFile(namaFile):
-    #inisialisasi awal 
-    coordinate = defaultdict(dict)
-    weightDictionary = defaultdict(dict)
-    #Baca File
-    dir = dirname(dirname(abspath(__file__)))
-    lokasiFile = os.path.join(dir, 'test\\' + namaFile + '.txt')
-    f = open(lokasiFile, "r")
-    #Mendapatkan list Seluruh Simpul 
-    listOfSimpul =  f.readline().replace("\n", "").split(" ")
-    nOfSimpul = len(listOfSimpul)
-    #Membentuk kamus kordinat 
-    location = f.readlines()
-    kordinate = location[:location.index("MATRIKS\n")]
-    for elem in kordinate: 
-        node = elem.replace("\n", "").split(" ")
-        coordinate[node[0]]["lat"] = float(node[1])
-        coordinate[node[0]]["long"] = float(node[2])
-    #Mengurus Adjacent Matriks
-    mat = []
-    Adjacent = location[location.index("MATRIKS\n") + 1:]
-    for elemenAdjacency in Adjacent:
-        elemen = elemenAdjacency.replace("\n", "").split(" ")
-        elemen = list(map(int, elemen))
-        mat.append(elemen)
-    for i in range(nOfSimpul):
-        for j in range(nOfSimpul):
-            if(mat[i][j] == 1):
-                weightDictionary[listOfSimpul[i]][listOfSimpul[j]] = eucleudianDistance(listOfSimpul[i], listOfSimpul[j], coordinate)
+    try:
+        # inisialisasi awal 
+        coordinate = defaultdict(dict)
+        weightDictionary = defaultdict(dict)
+        # Baca File
+        dir = dirname(dirname(abspath(__file__)))
+        lokasiFile = os.path.join(dir, 'test\\' + namaFile + '.txt')
+        with open(lokasiFile, "r") as f:
+            # Mendapatkan list Seluruh Simpul 
+            listOfSimpul =  f.readline().replace("\n", "").split(" ")
+            nOfSimpul = len(listOfSimpul)
+            # Membentuk kamus kordinat 
+            location = f.readlines()
+            kordinate = location[:location.index("MATRIKS\n")]
+            for elem in kordinate: 
+                node = elem.replace("\n", "").split(" ")
+                coordinate[node[0]]["lat"] = float(node[1])
+                coordinate[node[0]]["long"] = float(node[2])
+            # Mengurus Adjacent Matriks
+            mat = []
+            Adjacent = location[location.index("MATRIKS\n") + 1:]
+            for elemenAdjacency in Adjacent:
+                elemen = elemenAdjacency.replace("\n", "").split(" ")
+                elemen = list(map(int, elemen))
+                mat.append(elemen)
+            for i in range(nOfSimpul):
+                for j in range(nOfSimpul):
+                    if(mat[i][j] == 1):
+                        weightDictionary[listOfSimpul[i]][listOfSimpul[j]] = eucleudianDistance(listOfSimpul[i], listOfSimpul[j], coordinate)
 
-    return weightDictionary
+        return weightDictionary
+
+    except FileNotFoundError:
+        print("File tidak ditemukan. Silakan periksa kembali nama file yang dimasukkan.")
+        return None
+
 
 #TesFile
 #namaFile = str(input("Nama File tanpa ekstensi: "))
--- src/pencarian.py
+++ src/pencarian.py
@@ -1,37 +1,53 @@
 from helper import *
 #Pencarian dengan algoritma UCS 
 def UCS(loadDictionary, start, goal):
-    weight = {start : 0}   #Bobot yang selalu di update 
-    simpulnotVisited = []
-    visited = []
-    curr = start 
-    simpulnotVisited.append([curr,weight[curr]])
-    isTrue = True
-    while isTrue:
-        curr = min(simpulnotVisited, key=lambda x: x[1])
-        hasVisited = curr[0]
-        visited.append(curr)
-        simpulnotVisited.remove(curr)
-        #Jika sampai goal 
-        if(visited[-1][0] == goal):
-            isTrue = False
-        #Memeriksa apakah simpul anak sudah dikunjungi sebelumnya
-        for i in loadDictionary[hasVisited].items():
-            if i[0] in [j[0] for j in visited]:
-                continue
-            weight[i[0]] = weight[hasVisited] + i[1]
-            simpulnotVisited.append([i[0], weight[i[0]]])
-    shortPath = []
-    lastPath = goal 
-    shortPath.append(goal)
-    for i in range(len(visited) - 2, -1, -1):
-        check = visited[i][0]
-        if lastPath in [k[0] for k in loadDictionary[check].items()]:
-            if (weight[check] + loadDictionary[check][lastPath] == weight[lastPath]):
-                shortPath.append(check)
-                lastPath = check
-    shortPath.reverse()
-    return shortPath
+    try:
+        # penanganan eksepsi untuk simpul start dan goal
+        if start not in loadDictionary or goal not in loadDictionary:
+            raise ValueError("Start atau goal tidak ada dalam graf")
+
+        # inisialisasi variabel
+        weight = {start: 0}
+        simpulnotVisited = []
+        visited = []
+        curr = start
+        simpulnotVisited.append([curr, weight[curr]])
+        isTrue = True
+
+        # algoritma UCS
+        while isTrue:
+            curr = min(simpulnotVisited, key=lambda x: x[1])
+            hasVisited = curr[0]
+            #print(curr)
+            visited.append(curr)
+            simpulnotVisited.remove(curr)
+            if visited[-1][0] == goal:
+                isTrue = False
+            #Mengecek apakah simpul sudah dikunjungi, jika sudah akan dilewat
+            for i in loadDictionary[hasVisited].items():
+                if i[0] in [j[0] for j in visited]:
+                    continue
+                weight[i[0]] = weight[hasVisited] + i[1]
+                simpulnotVisited.append([i[0], weight[i[0]]])
+
+        # membangun jalur terpendek
+        shortPath = []
+        lastPath = goal
+        shortPath.append(goal)
+        for i in range(len(visited) - 2, -1, -1):
+            check = visited[i][0]
+            if lastPath in [k[0] for k in loadDictionary[check].items()]:
+                if weight[check] + loadDictionary[check][lastPath] == weight[lastPath]:
+                    shortPath.append(check)
+                    lastPath = check
+        shortPath.reverse()
+        return shortPath
+        
+    except ValueError as e:
+        print("Error: " + str(e))
+        
+    except:
+        print("Error : Coba Pastikan file dapat dibaca")
 
     
             
@@ -43,6 +59,3 @@ start = str(input("start: "))
 end = str(input("end: "))
 shortPath = UCS(test,start,end)
 print(shortPath)
-
-
-
